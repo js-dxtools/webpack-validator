724639da14c690c51d05a505f144c7f115061178
Nicolas LARCHE <nico060984@gmail.com>
2016-05-05 14:47:33 +0200

    feat(webpack-dev-server): Validate webpack-dev-server specific fields (#75)
    according to the source :
    https://github.com/webpack/webpack-dev-server/blob/master/bin/webpack-dev-server.js
    
    closes #21

---
+++ src/properties/devServer/index.js
@@ -0,0 +1,41 @@
+import Joi from 'joi'
+import watchOptionsSchema from '../watchOptions'
+import { notAbsolutePath, urlPart } from '../../types'
+
+export default Joi.object({
+  lazy: Joi.boolean(),
+  inline: Joi.boolean(),
+  stdin: Joi.boolean(),
+  open: Joi.boolean(),
+  info: Joi.boolean(),
+  quiet: Joi.boolean(),
+  https: Joi.boolean(),
+  key: Joi.string(),
+  cert: Joi.string(),
+  cacert: Joi.string(),
+  contentBase: [
+    Joi.object(),
+    Joi.array(),
+    Joi.string(),
+  ],
+  historyApiFallback: Joi.boolean(),
+  compress: Joi.boolean(),
+  port: Joi.number(),
+  public: Joi.string(),
+  host: Joi.string(),
+  publicPath: urlPart,
+  outputPath: urlPart,
+  filename: notAbsolutePath,
+  watchOptions: watchOptionsSchema,
+  hot: Joi.boolean(),
+  stats: Joi.object(),
+  noInfo: Joi.boolean(),
+  proxy: [
+    Joi.object(),
+    Joi.array(),
+    Joi.string(),
+  ],
+  staticOptions: Joi.object(),
+  headers: Joi.object(),
+})
+
