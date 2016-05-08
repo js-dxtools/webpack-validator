754fd720d72bead6da0302e5b17045b2b10b7b27
Jonathan Werner <jwerner@posteo.de>
2016-05-08 16:24:21 +0200

    fix: try fixing travis paths

--- test/passing-configs/angular2-webpack-starter.production.js
+++ test/passing-configs/angular2-webpack-starter.production.js
@@ -195,7 +195,7 @@ function gzipMaxLevel(buffer, callback) {
 
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [__dirname].concat(args));
+  return path.resolve.apply(path, [__dirname].concat(args));
 }
 
 function rootNode(args) {
--- test/passing-configs/angular2-webpack-starter.test.js
+++ test/passing-configs/angular2-webpack-starter.test.js
@@ -108,7 +108,7 @@ module.exports = {
 
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [__dirname].concat(args));
+  return path.resolve.apply(path, [__dirname].concat(args));
 }
 
 function rootNode(args) {
