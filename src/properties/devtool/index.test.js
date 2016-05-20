46c99a0f99f2a732e424dd4abbc55d3505fa2b88
Nicolas LARCHE <nico060984@gmail.com>
2016-05-20 16:51:07 +0200

    fix(devtool): devtool validation too lenient (#94)
    * fix: output.publicPath validation is too strict
    
    closes #88
    
    * fix: devtool validation is too lenient
    
    closes #86
    
    * Revert "fix: output.publicPath validation is too strict"
    
    This reverts commit da793cb0bfd78adf878554a27d84470d21891644.

--- src/properties/devtool/index.test.js
+++ src/properties/devtool/index.test.js
@@ -6,11 +6,21 @@ const validModuleConfigs = [
   'eval',
   '#@eval',
   '#cheap-module-eval-source-map',
+  'hidden-source-map',
+  'inline-source-map',
+  'eval-source-map',
+  'cheap-source-map',
 ]
 
 const invalidModuleConfigs = [
   // #0
   { input: 'foo', error: { } },
+  // #1
+  { input: 'eval-source-map-foo', error: { } },
+  // #2
+  { input: 'foo-cheap-source-map', error: { } },
+  // #3
+  { input: '#@eval-foo', error: { } },
 ]
 
 describe('devtool', () => {
