6967a661dbfbaa79c4389663cb428e140777ae5e
Michael Wolfenden <michael.wolfenden@gmail.com>
2016-06-29 03:17:20 +1000

    fix(cli): the --all-scripts flag should skip scripts that don't call the webpack cli (#115)
    * fix(cli): the --all-scripts flag should skip scripts that don't call the webpack cli
    
    111
    
    * Replaced `includes` with `indexOf`
    
    * Remove duplicate test script

--- src/bin/script-parsers.test.js
+++ src/bin/script-parsers.test.js
@@ -12,6 +12,46 @@ describe('webpack config name parser', () => {
       webpackConfig('webpack --profile --json > stats.json') === 'webpack.config.js'
     )
   })
+
+  it('should return undefined when script does not call the webpack cli', () => {
+    const sciptsThatDoNotCallWebpackCli = [
+      'karma start',
+      'istanbul check-coverage --statements 23 --branches 5 --functions 9 --lines 24',
+      'npm test -- --auto-watch --no-single-run',
+      'npm-run-all --parallel validate-webpack:* lint test --serial check-coverage',
+      'webpack-validator webpack.config.js --env.dev',
+      'webpack-validator webpack.config.js --env.prod',
+      'rimraf dist',
+      'cpy src/index.html src/favicon.ico dist',
+      'npm run clean-dist && npm run copy-files',
+      'npm run clean-and-copy',
+      'webpack-dev-server --env.dev --content-base dist',
+      'npm run clean-and-copy',
+      'eslint .',
+      'npm install && npm run validate',
+    ]
+
+    sciptsThatDoNotCallWebpackCli.forEach(script => {
+      assert(
+        webpackConfig(script) === undefined
+      )
+    })
+  })
+
+  it('should not return undefined when script calls the webpack cli', () => {
+    const scriptsThatCallWebpacCli = [
+      'webpack --env.dev',
+      'webpack --env.prod -p',
+      'NODE_ENV=development webpack --config demo.js',
+      'webpack --profile --json > stats.json',
+    ]
+
+    scriptsThatCallWebpacCli.forEach(script => {
+      assert(
+        webpackConfig(script) !== undefined
+      )
+    })
+  })
 })
 
 describe('node env parser', () => {
--- src/bin/validate-all-scripts.js
+++ src/bin/validate-all-scripts.js
@@ -6,6 +6,8 @@ module.exports = function validateAllScripts(scripts, quiet) {
     const script = scripts[name]
     const webpackConfigFile = parse.webpackConfig(script)
 
+    if (webpackConfigFile === undefined) return
+
     if (script.indexOf('SET NODE_ENV=') === 0 || script.indexOf('NODE_ENV=') === 0) {
       process.env.node_env = parse.nodeEnv(script)
     } else {
